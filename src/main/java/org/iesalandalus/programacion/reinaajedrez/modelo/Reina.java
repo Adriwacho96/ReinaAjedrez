960d045ec3fe53cc7ab1d402b6bff0312fd6cb41
Usuario <Usuario@DESKTOP-O8EUU3T>
2022-11-16 18:06:35 +0100

    Creo los movimientos de la pieza y el metodo toString

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -16,7 +16,8 @@ public class Reina {
 
 	public Reina(Color color) {
 		setColor(color);
-		}
+	}
+
 	private void setColor(Color color) {
 		if (color == null) {
 			throw new NullPointerException("ERROR: El color no puede ser nulo.");
@@ -40,6 +41,7 @@ public class Reina {
 		}
 		this.posicion = posicion;
 	}
+
 	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
 		if(pasos <1 || pasos >7) {
 			throw new IllegalArgumentException("ERROR: El número de pasos debe estar comprendido entre 1 y 7.");
@@ -47,6 +49,38 @@ public class Reina {
 		if(direccion==null) {
 			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
 			
+		}
+		try {
+			switch (direccion) {
+			case NORTE: 
+				setPosicion(new Posicion(posicion.getFila()+pasos, posicion.getColumna()));
+				break;
+			case NORESTE: 
+				setPosicion(new Posicion(posicion.getFila()+pasos,(char) (posicion.getColumna()+pasos)));
+				break;
+			case ESTE: 
+				setPosicion(new Posicion(posicion.getFila(),(char) (posicion.getColumna()+pasos)));
+				break;
+			case SURESTE: 
+				setPosicion(new Posicion(posicion.getFila()-pasos,(char) (posicion.getColumna()+pasos)));
+				break;
+			case SUR: 
+				setPosicion(new Posicion(posicion.getFila()-pasos,posicion.getColumna()));
+				break;
+			case SUROESTE: 
+				setPosicion(new Posicion(posicion.getFila()-pasos,(char)(posicion.getColumna()-pasos)));
+				break;
+			case OESTE: 
+				setPosicion(new Posicion(posicion.getFila(),(char)(posicion.getColumna()-pasos)));
+				break;
+			case NOROESTE: 
+				setPosicion(new Posicion(posicion.getFila()+pasos,(char) (posicion.getColumna()-pasos)));
+				break;
+			}
+		
+			
+		} catch (IllegalArgumentException e) {
+			throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
 		}
 	}
 

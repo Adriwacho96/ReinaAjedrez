ab8598d1648c2a28592d471ee7160d6183ec4225
Usuario <Usuario@DESKTOP-O8EUU3T>
2022-11-16 17:17:43 +0100

    Creo los metodos getset con los nulls y los colores con sus posiciones

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -1,6 +1,35 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Reina {
-private Color color;
-private Posicion posicion;
+	private Color color;
+	private Posicion posicion;
+
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if(color==null) {
+			throw new NullPointerException("ERROR: El color no puede ser nulo.");
+		}
+		if (color == Color.BLANCO) {
+			this.posicion = new Posicion(1, 'd');
+		}
+		if (color == Color.NEGRO) {
+			this.posicion = new Posicion(8, 'd');
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new NullPointerException("ERROR: La posición no puede ser nula");
+		}
+		this.posicion = posicion;
+	}
+
 }
